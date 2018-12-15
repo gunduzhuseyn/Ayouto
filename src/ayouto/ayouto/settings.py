7d2d95a7af5273836d1ab9ad4d9874fa5c44032c
Gunduz Huseyn Lee <gunduzhuseynli12@gmail.com>
2018-12-15 20:43:12 +0300

    db fix change from sqlite to postgressql

--- src/ayouto/ayouto/settings.py
+++ src/ayouto/ayouto/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ayouto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('db_name'),
+        'USER': os.environ.get('db_user'),
+        'PASSWORD': os.environ.get('db_password'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
