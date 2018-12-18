b5d22853f216e67f8edf193ff20f9a3ee8a3029c
Gunduz Huseyn Lee <gunduzhuseynli12@gmail.com>
2018-12-18 10:35:25 +0300

    post list and detail views implemented

--- src/ayouto/ecom/views.py
+++ src/ayouto/ecom/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from django.views.generic.edit import FormView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from .models import (CarPostModel,)
 
@@ -18,6 +20,24 @@ class CarPostCreateView(FormView):
         car_post.save()
 
         return super().form_valid(form)
+
+
+class CarPostDetailView(DetailView):
+    model = CarPostModel
+    template_name = 'ecom/car_post_detail.html'
+    context_object_name = 'car_post'
+
+
+class CarPostListView(ListView):
+    model = CarPostModel
+    paginate_by = 25
+    template_name = 'ecom/car_post_list.html'
+    context_object_name = 'car_post_list'
+    ordering = ['-post_time']
+
+    def get_queryset(self):
+        return CarPostModel.objects.filter(is_sold=False).order_by('-post_time')
+
 
 
 
